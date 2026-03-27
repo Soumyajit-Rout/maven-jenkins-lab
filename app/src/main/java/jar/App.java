1536466a3f789156aa31c90c98c4d68bf9270a8f
Soumyajit-Rout <115661404+Soumyajit-Rout@users.noreply.github.com>
2026-03-27 08:36:41 +0530

    Update App.java

--- app/src/main/java/jar/App.java
+++ app/src/main/java/jar/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World devops hii cloud github webhook!" );
+        System.out.println( "Hello World devops hii cloud github webhook add !" );
     }
 }
